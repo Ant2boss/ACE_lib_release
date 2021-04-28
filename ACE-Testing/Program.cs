37b9f297e2cd116c01f92f76e592102e489ae291
SKlasic <sklasich@gmail.com>
2021-04-28 20:05:18 +0200

    Vec2 basic testing

--- ACE-Testing/Program.cs
+++ ACE-Testing/Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using ACE_lib.Vectors;
+
 namespace ACE_Testing
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			Vec2i vec1 = new Vec2i(3, 9);
+
+			Vec2i vec2 = new Vec2i(7, 2);
+
+			Console.WriteLine($"{vec1} + {vec2} = {vec1 + vec2}");
 		}
 	}
 }
