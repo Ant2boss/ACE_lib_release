b59de17cc8069e2250579d410a65d7fab51419ff
SKlasic <sklasich@gmail.com>
2021-05-20 20:38:08 +0200

    Started work on ACE_lib_2

---
+++ ACE_lib2/Content/Interfaces/IMatrix2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ACE_2D_Base.Vectors;
+
+namespace ACE_lib2.Content.Interfaces
+{
+	public interface IMatrix2<T>
+	{
+		void SetAt(T el, int x, int y);
+		void SetAt(T el, Vec2i Index);
+
+		T GetAt(int x, int y);
+		T GetAt(Vec2i Index);
+
+		void Clear(T ClearWith);
+
+		Vec2i GetSize();
+	}
+}
